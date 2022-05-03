3101cdc2b18c077995af7405d9d4bf7fa6c5d7e3
Miguel Peixoto - UM <92986801+pg45517@users.noreply.github.com>
2022-05-03 09:48:17 +0100

    fixed multithreading, get size of files

--- server/server.py
+++ server/server.py
@@ -1,33 +1,39 @@
 #! /usr/bin/python3
+from asyncore import file_dispatcher
 import socket
 import os
 import sys
 import threading
 
 TTL = 5
+#IP_ADDR = '2001:0690:2280:0820:33::2'
 IP_ADDR = '::1'
 UDP_PORT = 9999
 BUFFER_SIZE = 1024
+FILES_FOLDER = '/home/core/file-share/server/files'
 
 def get_files():
-    file_list = os.listdir('files')
-    final = "file-list: "
-    return final.join(file_list)
+    file_dict = {}
+    file_list = os.listdir(FILES_FOLDER)
+    for file in file_list:
+        file_dict[file] = os.path.getsize(os.path.join(FILES_FOLDER,file))
+    return "file-list:\n{}".format(file_dict)
 
-def handle_client(connection, file_name, client_addr):
+def handle_client(file_name, client_addr):
     """ Check if requested file exists and send it to client
     Args:
         connect: socket
         file_name: requested file
         client_addr: tupple with (ip,port)
     """
+    connection = socket.socket(socket.AF_INET6, socket.SOCK_DGRAM)
     if file_name.decode('utf-8') == 'file-list':
         list_of_files = get_files()
         print('Sending list fo files:\n {}'.format(list_of_files))
         connection.sendto(list_of_files.encode(), client_addr)
         return
-    
-    file_path = os.path.join('files', file_name.decode('utf-8'))
+
+    file_path = os.path.join(FILES_FOLDER, file_name.decode('utf-8'))
     if not os.path.isfile(file_path):
         print("{} not found".format(file_path))
         connection.sendto("file-not-found".encode(), client_addr)
@@ -59,8 +65,8 @@ def main():
     while True:
         try:
             request, client_addr = s.recvfrom(BUFFER_SIZE)
-            #print(request.decode('utf8'))
-            threading.Thread(target=handle_client(s,request,client_addr)).start()
+            print(request.decode('utf8'))
+            threading.Thread(target=handle_client(request,client_addr)).start()
         except KeyboardInterrupt:
             s.close()
             sys.exit(0)
