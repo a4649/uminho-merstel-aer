1cd01a37463ee1a6f3d04789f09449919a413682
Miguel Peixoto - UM <92986801+pg45517@users.noreply.github.com>
2022-05-13 16:37:50 +0100

    code that really works!

--- server/server.py
+++ server/server.py
@@ -4,13 +4,23 @@ import socket
 import os
 import sys
 import threading
+import math
+import hashlib
 
 TTL = 5
 #IP_ADDR = '2001:0690:2280:0820:33::2'
 IP_ADDR = '::1'
-UDP_PORT = 9999
-BUFFER_SIZE = 1024
+UDP_CONTROL_PORT = 9999
+UDP_DATA_PORT = 9991
+BUFFER_SIZE = 2048
+BLOCK_SIZE = 960
 FILES_FOLDER = '/home/core/file-share/server/files'
+
+control_socket = socket.socket(socket.AF_INET6, socket.SOCK_DGRAM)
+data_socket = socket.socket(socket.AF_INET6, socket.SOCK_DGRAM)
+
+def get_checksum(bdata):
+    return hashlib.md5(bdata).hexdigest()
 
 def get_files():
     file_dict = {}
@@ -19,56 +29,145 @@ def get_files():
         file_dict[file] = os.path.getsize(os.path.join(FILES_FOLDER,file))
     return "file-list:\n{}".format(file_dict)
 
-def handle_client(file_name, client_addr):
+def get_file_size(file):
+    return os.path.getsize(file)
+
+def send_data(data, addr):
+    tmp_socket = socket.socket(socket.AF_INET6, socket.SOCK_DGRAM)
+    tmp_socket.sendto(data.encode(), addr)
+
+def check_file(file):
+    if not 'home' in file:
+        file = os.path.join(FILES_FOLDER,file)
+    return os.path.isfile(os.path.join(FILES_FOLDER, file))
+
+def send_file_info(file, addr):
+    if not check_file(file):
+        send_data("file-not-found", addr)
+        sys.exit()
+    tmp_file = os.path.join(FILES_FOLDER,file)
+    #print("Getting info for file: " + tmp_file)
+    size_of_file = get_file_size(tmp_file)
+    tmp_seq = size_of_file / BLOCK_SIZE
+    blocks = math.ceil(tmp_seq)
+    lista = []
+    file_dict ={}
+    tmp_file = open(tmp_file,'rb')
+    for i in range(blocks):
+        tmp_data = tmp_file.read(BLOCK_SIZE)
+        lista.append(tmp_data)
+        file_dict[i] = get_checksum(tmp_data)
+    tmp_file.close()
+    tmp_response = 'file-info:' + file + ':' + str(len(file_dict))
+    #tmp_response = 'file-info:' + file + ':' + str(len(lista))
+    print('REPLY: {}'.format(tmp_response))
+    tmp_socket = socket.socket(socket.AF_INET6, socket.SOCK_DGRAM)
+    tmp_socket.sendto(tmp_response.encode(), addr) 
+
+def send_block_info(file, block, addr):
+    if not check_file(file):
+        send_data("file-not-found", addr)
+        sys.exit()
+    tmp_file = os.path.join(FILES_FOLDER,file)
+    size_of_file = get_file_size(tmp_file)
+    tmp_seq = size_of_file / BLOCK_SIZE
+    blocks = math.ceil(tmp_seq)
+    file_dict ={}
+    tmp_file = open(tmp_file,'rb')
+    for i in range(blocks):
+        tmp_data = tmp_file.read(BLOCK_SIZE)
+        file_dict[i] = get_checksum(tmp_data)
+    tmp_file.close()
+
+    if int(block) < len(file_dict):
+        tmp_response = 'block-info:' + file + ':' + block + ':' + file_dict[int(block)]
+        print('REPLY: {}'.format(tmp_response))
+        tmp_socket = socket.socket(socket.AF_INET6, socket.SOCK_DGRAM)
+        tmp_socket.sendto(tmp_response.encode(), addr)
+    else:
+        tmp_socket = socket.socket(socket.AF_INET6, socket.SOCK_DGRAM)
+        tmp_socket.sendto("Invalid block number".encode(), addr)
+
+def send_data_block(file, seq, addr):
+    if not check_file(file):
+        send_data("file-not-found", addr)
+        sys.exit()
+    #print("Sending {} block number {}".format(file, seq))
+    tmp_socket = socket.socket(socket.AF_INET6, socket.SOCK_DGRAM)
+    tmp_file = os.path.join(FILES_FOLDER,file)
+    size_of_file = get_file_size(tmp_file)
+    tmp_seq = size_of_file / BLOCK_SIZE
+    blocks = math.ceil(tmp_seq)
+    lista = []
+    read_file = open(tmp_file,'rb')
+    for i in range(blocks):
+        lista.append(read_file.read(BLOCK_SIZE))
+    read_file.close()
+    tmp_header = (file + ';' + seq + ';')
+    #print('TOTAL BLOCOS:{}'.format(len(lista)))
+    #print('REPLY: {}{}'.format(tmp_header, lista[int(seq)]))
+    print('REPLY: {}{}'.format(tmp_header, "***binary data***"))
+    x = tmp_socket.sendto(tmp_header.encode() + lista[int(seq)], addr)
+    print("SENT: {} bytes".format(x))
+
+def handle_client(request, client_addr):
     """ Check if requested file exists and send it to client
     Args:
         connect: socket
         file_name: requested file
         client_addr: tupple with (ip,port)
     """
-    connection = socket.socket(socket.AF_INET6, socket.SOCK_DGRAM)
-    if file_name.decode('utf-8') == 'file-list':
+    tmp_socket = socket.socket(socket.AF_INET6, socket.SOCK_DGRAM)
+    tmp_request = request.decode('utf-8')
+
+    # handle list of available files request
+    if  tmp_request == 'file-list':
         list_of_files = get_files()
-        print('Sending list fo files:\n {}'.format(list_of_files))
-        connection.sendto(list_of_files.encode(), client_addr)
+        print('REPLY: {}'.format(list_of_files))
+        tmp_socket.sendto(list_of_files.encode(), client_addr)
         return
 
-    file_path = os.path.join(FILES_FOLDER, file_name.decode('utf-8'))
-    if not os.path.isfile(file_path):
-        print("{} not found".format(file_path))
-        connection.sendto("file-not-found".encode(), client_addr)
+    # handle file info request
+    if 'file-info' in tmp_request:
+        tmp_file = tmp_request.split(':')[1]
+        send_file_info(tmp_file, client_addr)
         return
-    else:
-        connection.sendto("file-found".encode(), client_addr)
+        
+    # handle file block request
+    if 'file-data' in tmp_request:
+        tmp_file = tmp_request.split(':')[1]
+        tmp_block = tmp_request.split(':')[2]
+        send_data_block(tmp_file, tmp_block,client_addr)
+        return
 
-    file = open(file_path,'rb')
-    data = file.read(BUFFER_SIZE)
-    while data:
-        if(connection.sendto(data, client_addr)):
-            print("Sending {} to {}...".format(file_path,client_addr[0]))
-            data = file.read(BUFFER_SIZE)
-    file.close()
-    print("Done!")
-
+    if 'block-info' in tmp_request:
+        tmp_file = tmp_request.split(':')[1]
+        tmp_block = tmp_request.split(':')[2]
+        send_block_info(tmp_file, tmp_block,client_addr)
+        return
+ 
+    # invalid request, kill thread
+    send_data('Invalid request', client_addr)
+    return
+        
 def main():
     try:
-        s = socket.socket(socket.AF_INET6, socket.SOCK_DGRAM)
-        s.bind((IP_ADDR, UDP_PORT))
-        print("Accepting clients on {}:{}...".format(IP_ADDR, UDP_PORT))
+        control_socket.bind((IP_ADDR, UDP_CONTROL_PORT))
+        data_socket.bind((IP_ADDR, UDP_DATA_PORT))
+        print("Accepting clients on {}:{}...".format(IP_ADDR, UDP_CONTROL_PORT))
         print("Press Ctrl+C to terminate")
-        # s.setblocking(0)
-        # s.settimeout(15)
     except socket.error:
-        print("Failed to bind on {}:{}...".format(IP_ADDR, UDP_PORT))
+        print("Failed to bind on {}:{}...".format(IP_ADDR, UDP_CONTROL_PORT))
         sys.exit(1)
 
     while True:
         try:
-            request, client_addr = s.recvfrom(BUFFER_SIZE)
-            print(request.decode('utf8'))
+            request, client_addr = control_socket.recvfrom(BUFFER_SIZE)
+            print('REQUEST: {}'.format(request.decode('utf8')))
             threading.Thread(target=handle_client(request,client_addr)).start()
         except KeyboardInterrupt:
-            s.close()
+            control_socket.close()
+            data_socket.close()
             sys.exit(0)
 
 
